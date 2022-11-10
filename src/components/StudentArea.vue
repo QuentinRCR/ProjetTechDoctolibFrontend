977e8de4ed6db9c8c87852192f28925ddbd4734b
QuentinRCR <reyquentin.rq@gmail.com>
2022-11-10 17:35:55 +0100

    add navigation menu

---
+++ src/components/StudentArea.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Section élève</h1>
+</template>
+
+<script>
+export default {
+    name: 'StudentArea',
+    data: function() {
+      return {
+      }
+    },
+    components: {
+    },
+    methods: {
+        /*
+      SubmitForm(CommunicationMean,Date){
+        console.log("faire la prise de rendez-vous"); 
+        console.log(`${Date}`);
+        this.$emit('close-popup');
+      }*/
+    }
+  }
+  </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,93 @@
-<script setup>
-//import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <div class="home">
-    <h1>This is an home page</h1>
-    <button @click="LogOut">Se déconnecter</button>
-    <button @click="ToggleAppoi">Prendre un rendez-vous</button>
-    <MakeAppoi @close-popup="ToggleAppoi" v-if="isAddAppointement"></MakeAppoi>
+    <div class="SideMenu">
+      <MainNavigation @panel-change="updatePanel"></MainNavigation>
+      <button @click="LogOut">Se déconnecter</button>
+    </div>
+    <div class="content">
+      <div class="TopMenu">
+        <button @click="ToggleAppoi">Prendre un rendez-vous</button>
+      </div>
+      <MakeAppoi @close-popup="ToggleAppoi"  v-if="isAddAppointement"></MakeAppoi>
+      <div class="pannels">
+        <StudentArea class="pannel" v-if="currentPanel.name === 'panelA'"></StudentArea>
+        <MyAccount classe="pannel" v-if="currentPanel.name === 'panelB'"></MyAccount>
+      </div>
+    </div>
+
+    
   </div>
 </template>
 
 <script>
 import MakeAppoi from '../components/MakeAppoi.vue';
+import MainNavigation from '../components/MainNavigation.vue';
+import StudentArea from '../components/StudentArea.vue';
+import MyAccount from '../components/MyAccount.vue'
 
 export default {
   name: 'HomePage',
   components: {
-    MakeAppoi
+    MakeAppoi,
+    MainNavigation,
+    StudentArea,
+    MyAccount
   },
   data: function() {
     return {
-      isAddAppointement: false
+      isAddAppointement: false,
+      currentPanel: {}
     }
   },
   methods: {
+    ToggleAppoi(){
+      this.isAddAppointement = this.isAddAppointement != true;
+    },
+    updatePanel(newPanel) {
+      this.currentPanel = newPanel;
+    },
     LogOut(){
       console.log("bien déconnecter la personne");
       this.$router.push("/");
     },
-    ToggleAppoi(){
-      this.isAddAppointement = this.isAddAppointement != true;
-    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.home{
+  display: flex;
+  border: solid hotpink;
+
+  .SideMenu{
+    border: solid red;
+    width: 10%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 680px;
+  }
+
+  .content{
+    width: 90%;
+    border: solid cadetblue;
+    .pannels{
+      border: solid palegreen;
+   }
+
+   .TopMenu{
+    width: 50px;
+   }
+
+   .pannel{
+    height: 570px;
+   }
+  }
+   
+}
+
 </style>
+
+
+
